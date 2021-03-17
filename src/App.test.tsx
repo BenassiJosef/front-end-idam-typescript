f5b82ae1bf03d950665df8929c6fddf24ae05c4a
BenassiJosef <benassijosef@gmail.com>
2021-03-17 18:57:18 +0000

    action creator test and mock store

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore({});
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = screen.getByText(/Click Me/i);
   expect(linkElement).toBeInTheDocument();
 });
