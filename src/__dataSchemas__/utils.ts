d0398cc11af7521ff6ea341a281ef3e787a4a823
BenassiJosef <benassijosef@gmail.com>
2021-03-29 23:52:43 +0100

    now throwing err messages from ajv on schema fail

--- src/__dataSchemas__/utils.ts
+++ src/__dataSchemas__/utils.ts
@@ -1,14 +1,16 @@
 import ajv from "./validation";
 
 const scrubPayload = <T>(payload: T, schemaName: string): boolean => {
-  const validate = ajv.getSchema(schemaName);
+  const validate = ajv.validate(schemaName, payload);
+  const errs = ajv.errors;
 
-  if (!validate) throw new Error("getSchema returned undefined");
-
-  // valid will return true or false based on the ajv api
-  const valid = validate(payload);
-
-  if (!valid) throw new Error("validate schema returned false");
+  if (!validate) {
+    if (errs) {
+      // can use these error messages to display to the user. Maybe work on a Errors redux action creator and component
+      throw new Error(errs[0].message);
+    }
+    throw new Error("validating schema resulted in Error");
+  }
 
   return true;
 };
