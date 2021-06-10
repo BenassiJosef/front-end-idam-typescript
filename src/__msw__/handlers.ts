54f80652c8c6e2fa0c618fa699425a888e192392
BenassiJosef <benassijosef@gmail.com>
2021-06-10 02:46:48 +0100

    add origin to msw

--- src/__msw__/handlers.ts
+++ src/__msw__/handlers.ts
@@ -2,10 +2,13 @@ import { rest } from "msw";
 import IdamApiUrl from "../utils";
 
 const handlers = [
-  rest.post(`${IdamApiUrl()}/register`, (req, res, ctx) => {
-    return res(ctx.status(200));
-  }),
-  rest.post(`${IdamApiUrl()}/verify`, (req, res, ctx) => {
+  rest.post(
+    `${IdamApiUrl(window.location.origin)}/register`,
+    (req, res, ctx) => {
+      return res(ctx.status(200));
+    }
+  ),
+  rest.post(`${IdamApiUrl(window.location.origin)}/verify`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];
